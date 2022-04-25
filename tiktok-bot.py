54e575bc9e3452e5f724ae3dcfacd9ca54e7683c
dsol_cpu <dmsolinsky@email.wm.edu>
2022-04-25 07:05:41 -0400

    what i have

--- tiktok-bot.py
+++ tiktok-bot.py
@@ -1,8 +1,12 @@
+from asyncio.windows_events import NULL
+from concurrent.futures import thread
+from numpy import empty
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import json
 import random
 import os
+import time
+
 
 data = open("settings.json", "r+", encoding="utf8")
 jsondata = json.load(data)
@@ -12,11 +16,10 @@ data.close()
 # Just make variables first, can make dummy information later
 firstName = jsondata["firstName"]
 lastName = jsondata["lastName"]
-userName = jsondata["userName"] + str(random.randint(10000, 99999))
+userName = ""
 emailEnd = jsondata["emailEnd"]
 passw = jsondata["passw"]
 email = jsondata["email"]
-emailFull = email + "+" + str(random.randint(10000, 99999)) + "@" + emailEnd
 
 browser = webdriver.Chrome(executable_path=os.getcwd() + "/chromedriver")
 browser.implicitly_wait(10)
@@ -32,83 +35,113 @@ def saveSuccessfulInfo():
 def getEmailCode(): #Go to gmail and save the confirmation code from the most recent email
     return
 
-def getPhoneCode():
-    browser.get("https://freepublicsms.com/page/Fake-Phone-Number-For-Verification")
-    browser.implicitly_wait(1)
-    fakePhoneTab = browser.current_window_handle
+def createEmailAlias():
+    # use one existing email address
+    # create a nickname for the email address
 
-    fake_phone_num = browser.find_element_by_xpath("/html/body/div[2]/div[2]/div/div/div[1]/div/h2").text()
-    return fake_phone_num
+    fullEmail = ""
+    browser.execute_script("window.open('about:blank', '');")
+    browser.switch_to.window(browser.window_handles[1])
+    browser.get('https://account.protonmail.com/signup')
+    time.sleep(20)
 
-def createEmailAccount():
-    browser.get("https://accounts.google.com/signup")
-    browser.implicitly_wait(1)
-    gmailTab = browser.current_window_handle
-    try:
-        #get the first name textbox
-        first_name = browser.find_element_by_id("firstName")
-        first_name.send_keys(firstName)
+    browser.find_element_by_id("username").send_keys(userName)
+    print("username input")
 
-        surname = browser.find_element_by_id("lastName")
-        surname.send_keys(lastName)
+    browser.find_element_by_id("password").send_keys(passw)
+    print("password input")
 
-        username = browser.find_element_by_id("username")
-        username.send_keys(userName)
-        
-        password = browser.find_element_by_id("passwd")
-        password.send_keys(passw)
+    browser.find_element_by_id("repeat-password").send_keys(passw)
+    browser.find_element_by_class_name('button button-large button-solid-norm w100 mt1-75').click()
+    browser.find_element_by_class_name('button button-large button-ghost-norm w100 mt0-5').click()
+    browser.switch_to_window(browser.window_handles[0])
 
-        confirm_password = browser.find_element_by_id("confirm-passwd")
-        confirm_password.send_keys(passw)
+    # browser.get("https://account.protonmail.com/signup")
+    # userName = jsondata["userName"] + str(random.randint(10000, 99999))
 
-        next_btn = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button")
-        next_btn.click()       
+    # browser.implicitly_wait(5)
 
-        phone_number = browser.find_element_by_id("phoneNumberId")
-        phone_number.send_keys(getPhoneCode())
+    # if browser.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/main/div[2]/div/svg').is_displayed():
+    #     browser.implicitly_wait(5)
 
-        next_btn = browser.find_element_by_xpath('//*[@id="accountDetailsNext"]/div/button')
-        next_btn.click()
+    # #     browser.implicitly_wait(1)
+    # # #get the first name textbox
+    # # first_name = browser.find_element_by_id("firstName")
+    # # first_name.send_keys(firstName)
 
-        if(browser.find_element_by_xpath('//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[2]/div[1]/div/div[2]/div[2]/div/text()').text() == "That username is taken. Try another."):
-            userName = browser.find_element_by_xpath('//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[2]/div[2]/div/ul/li[2]/button').text()
-            browser.find_element_by_xpath('//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[2]/div[2]/div/ul/li[2]/button').click()
-            next_btn.click()
+    # # surname = browser.find_element_by_id("lastName")
+    # # surname.send_keys(lastName)
 
-    except:
-        raise ValueError("Textbox not found")
-    return 
+    # user_name = browser.find_element_by_id("username")
+    # user_name.send_keys(userName)
+    
+    # password = browser.find_element_by_id("password")
+    # password.send_keys(passw)
+
+    # confirm_password = browser.find_element_by_id("repeat-password")
+    # confirm_password.send_keys(passw)
+
+    # next_btn = browser.find_element_by_xpath("/html/body/div[1]/div[3]/div/div/main/div[2]/form/button")
+    # next_btn.click()       
+
+    # while browser.find_element_by_xpath('/html/body/div[1]/label/div[3]/svg').is_displayed():
+    #     userName = jsondata["userName"] + str(random.randint(10000, 99999))
+    #     user_name.send_keys(userName)
+    #     next_btn.click()
+    # browser.implicitly_wait(5)
+
+    # browser.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/main/div[2]/form/button[2]').click()
+    # browser.find_element_by_xpath('/html/body/div[4]/dialog/div/div[3]/button[1]').click()
+    # browser.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/main/div[2]/div[2]/div[1]/button').click()
+    # browser.find_element_by_id('checkbox').click()
+
+    # browser.implicitly_wait(5)
+    # browser.find_element_by_xpath('/html/body/div[4]/dialog/form/div/div/footer/button').click()
+    # browser.find_element_by_xpath('/html/body/div[4]/dialog/form/div/div/footer/button').click()
+
+    # return userName + "@protonmail.com"
+
+def randomSleepInterval():
+    randomInt = random.randint(range(1,5))
+    return randomInt
+
 
 def main():
-    try:
-        #create account
-        browser.get("https://www.tiktok.com/signup/phone-or-email/email")
-        tiktokTab = browser.current_window_handle
+    #create account
+    browser.get("https://www.tiktok.com/signup/phone-or-email/email")
+    tiktokTab = browser.current_window_handle
+    emailPath = browser.find_element_by_name("email")
+    passPath = browser.find_element_by_name("password")
 
-        emailPath = browser.find_element_by_name("email")
-        passPath = browser.find_element_by_name("password")
+    passPath.send_keys(passw)
+    print("email and password entered.")
+    browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[1]/div").click()
+    time.sleep(3)
+    browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[1]/ul/li[5]").click()
+    time.sleep(5)
+    browser.find_element_by_xpath("/html/body/div[1]/div").click()
+    time.sleep(2)
+    browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[2]/div").click()
+    time.sleep(7)
 
-        emailPath.send_keys(emailFull)
-        passPath.send_keys(passw)
-        print("email and password entered.")
-        browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[1]/div").click()
-        browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[1]/ul/li[5]").click()
-        browser.find_element_by_xpath("/html/body/div[1]/div").click()
-        browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[2]/div").click()
-        browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[2]/ul/li[5]").click()
-        browser.find_element_by_xpath("/html/body/div[1]/div").click()
-        browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[3]/div").click()
-        browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[3]/ul/li[29]").click() #age
-        browser.find_element_by_xpath("/html/body/div[1]/div").click()
-        createEmailAccount()
-        #fill in code
-        browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[4]/div[5]/button").click() #send code button
+    browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[2]/ul/li[5]").click()
+    time.sleep(2)
 
-    except:
-        print("Webpage elements have changed.")
+    browser.find_element_by_xpath("/html/body/div[1]/div").click()
+    time.sleep(3)
 
-    #get code from newly created email account at gmail.com
-       
+    browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[3]/div").click()
+    time.sleep(6)
+
+    browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[3]/ul/li[29]").click() #age
+    time.sleep(8)
+
+    browser.find_element_by_xpath("/html/body/div[1]/div").click()
+    emailPath.send_keys("zd9393960@gmail.com")
+
+    #fill in code
+    browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[4]/div[5]/button").click() #send code button
+    #get code from newly created email account at gmail.com       
 
 
 if __name__ == "__main__":
