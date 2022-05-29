b0b0dc66dfb71c1967b2dbefdefaa7589bd45cad
dsol_cpu <dmsolinsky@email.wm.edu>
2022-05-28 20:33:32 -0400

    update

---
+++ emailCreate.py
@@ -0,0 +1,32 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import undetected_chromedriver as uc
+import csv
+import os
+
+
+# Attempt to create email using lastmx
+# Currently, lastmx reads a "SameSite" cookie error when trying this code
+def main():
+    f = open(os.getcwd()+"/emailList.csv", 'w')
+    writer = csv.writer(f)
+    browser = uc.Chrome()
+    browser.delete_all_cookies()
+    browser.get("https://lastmx.com/")
+    
+    for i in range(10):
+        browser.find_element(By.ID, "email").send_keys("malynvirth2@gmail.com")
+        browser.implicitly_wait(2)
+        browser.find_element(By.XPATH, "/html/body/div/div[1]/main/div[1]/div/div/form/div/div/div[1]/div/button").click()
+        time.sleep(10)
+        emailAlias = browser.find_element(By.CLASS_NAME, "form-control").text
+        writer.writerow(emailAlias)
+        browser.refresh()
+        browser.implicitly_wait(2)
+
+    f.close()
+
+if __name__ == "__main__":
+    main()
--- tiktok account creation/tiktok-bot.py
+++ tiktok account creation/tiktok-bot.py
@@ -1,146 +1,144 @@
-from asyncio.windows_events import NULL
-from concurrent.futures import thread
+from logging import exception
+import os
 import time
-from typing import KeysView
-from weakref import proxy
-from numpy import empty
-from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
 import undetected_chromedriver as uc
-import json
-import google_auth_httplib2
+import json, csv
 
+class My_Chrome():
+    def __init__(self, jsondata, csvdata=[], zoho=False):
+       
+        self.firstName = jsondata["firstName"]
+        self.lastName = jsondata["lastName"]
+        self.emailEnd = jsondata["emailEnd"]
+        self.passw = jsondata["passw"]
+        self.email = jsondata["email"]
+        self.fullemail = self.email + "@" + self.emailEnd
+        self.csvdata = csvdata
 
-class My_Chrome(uc.Chrome):
+        #TODO: turn off the SameSite cookie
+
+        self.browser = uc.Chrome()
+        self.browser.delete_all_cookies()
+        self.zoho = zoho
+
     def __del__(self):
         pass
 
-    # make 1 gmail account
-    # automate alias creation on the same email
-    # save all aliases into json and temporary list
-    # go to tiktok and loop creation through all aliases, each alias creating a new email
-
-    # after successful registration, register gmail account and password + tiktok account and password in 1 json node 
-
-    # Functions
-
-    def saveSuccessfulInfo():
+    def saveSuccessfulInfo(self, successList):
+        f = open(os.getcwd()+"/emailList.csv", 'w')
+        writer = csv.writer(f)
+        writer.writerows(successList)
         #tiktok and linked gmail account info
         #tiktok user and password
         #gmail user and password
+        f.close()
         return
 
-    def getEmailCode(fullEmail, password): #Go to gmail and save the confirmation code from the most recent email
-        
-        return
+    def tiktokAccountCreate(self):
+        success = False
+        iter = 0
+        for i in range (len(self.csvdata)):
+            self.browser.get("https://www.tiktok.com/signup/phone-or-email/email")
+            self.browser.implicitly_wait(10)
 
-    def createEmailAlias(browser, gmailAddress, passw):
-
-
-    #TODO:
-
-    # - log into domain
-    # - make alias
-    # - return alias
-
-        emailAlias = ""
-        stall = input("Stalling")
-        return emailAlias
-
-    def tiktokAccountCreate(browser):
-        return
-
-    def main():
-        try:
-            data = open("settings.json", "r+", encoding="utf8")
-            jsondata = json.load(data)
-            data.close()
-
-            # Import info subject to change
-            # Just make variables first, can make dummy information later
-
-            firstName = jsondata["firstName"]
-            lastName = jsondata["lastName"]
-            emailEnd = jsondata["emailEnd"]
-            passw = jsondata["passw"]
-            email = jsondata["email"]
-            fullemail = email + "@" + emailEnd
-
-            option = uc.ChromeOptions()
-            browser = uc.Chrome()
-            browser.get("https://www.tiktok.com/signup/phone-or-email/email")
-            browser.implicitly_wait(10)
-            time.sleep(2)
-            
-            emailPath = browser.find_element_by_name("email")
-            passPath = browser.find_element_by_xpath("//input[@type = 'password']")
-            passPath.send_keys(passw)
+            WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((By.NAME, "email")))
+            emailPath = self.browser.find_element(By.NAME, "email")
+            emailPath.send_keys(self.email)
+            passPath = self.browser.find_element(By.CSS_SELECTOR, "input[type = 'password']")        
+            passPath.send_keys(self.passw)
             print("email and password entered.")
-
-            browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[1]/div").click()
-            browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[1]/ul/li[5]").click()
-            browser.find_element_by_xpath("/html/body/div[1]/div").click()
-            browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[2]/div").click()
-            browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[2]/ul/li[5]").click()
-            browser.find_element_by_xpath("/html/body/div[1]/div").click()
-            browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[3]/div").click()
-            browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/form/div[2]/div[3]/ul/li[29]").click() #age
-            browser.find_element_by_xpath("/html/body/div[1]/div").click()
-
-            #TODO:
-            #1. need email address
-            #2. log into zoho and create email aliases with Selenium
-
-            browser.execute_script("window.open('https://accounts.zoho.com/signin?servicename=VirtualOffice&signupurl=https://www.zoho.com/mail/zohomail-pricing.html&serviceurl=https://mail.zoho.com');")
-            #proceed with new tab
-            browser.switch_to.window(browser.window_handles[1])
-            browser.implicitly_wait(10)
-
-            browser.find_element_by_name('LOGIN_ID').send_keys(email)
-            browser.find_element_by_id('nextbtn').click()
+            month = WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[1]/form/div[2]/div[1]/div')))
+            if month:
+                self.browser.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/form/div[2]/div[1]/div').click()
+                self.browser.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/form/div[2]/div[1]/ul/li[5]').click()
             
-            browser.execute_script("window.open('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin');") 
-            #proceed with new tab
-            browser.switch_to.window(browser.window_handles[2])
-            browser.implicitly_wait(10)
-
-            gmailUserBox = browser.find_element_by_id('identifierId')
-            gmailUserBox.send_keys(email)        
-            browser.find_element_by_xpath('//*[@id="identifierNext"]/div/button').click()
-            browser.implicitly_wait(10)
-
-            gmailPasswBox = browser.find_element_by_name('password')
-            gmailPasswBox.send_keys(passw)
-            browser.find_element_by_xpath('//*[@id="passwordNext"]/div/button').click()
-            browser.implicitly_wait(10)
-
-            time.sleep(10)
-            browser.refresh()
-            browser.find_element_by_xpath("/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div[8]/div/div[1]/div[3]/div/table/tbody/tr[1]/td[5]/div[2]/span[1]/span").click()
-
-            #TODO: 
-            #Find right iframe
+            day = WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[1]/form/div[2]/div[2]/div')))
+            if day: 
+                self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/form/div[2]/div[2]/div").click()
+                self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/form/div[2]/div[2]/ul/li[3]").click()
             
+            year = WebDriverWait(self.browser, 20).until(ec.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[1]/form/div[2]/div[3]/div")))
+            if year:
+                self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/form/div[2]/div[3]/div").click()
+                self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/form/div[2]/div[3]/ul/li[40]").click()
+                #possibly randomize year
+        self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/form/div[4]/div[5]/button").click()        
+        self.browser.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/form/div[4]/div[5]/div/input').send_keys(self.getTikTokCode())
+        
 
-            zohoOTPCode = browser.find_element_by_xpath('//*[@id=":rk"]/div[1]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[4]/td/b').text
-            browser.find_element_by_xpath('//*[@id=":4"]/div[2]/div[1]/div/div[2]/div[3]').click()
+    #Google Signin
+    def googleSignIn(self):
+        if self.zoho:
+            self.zohoSignIn()
+            zohoOTPCode = self.getZohoCode()
+                    
+            self.browser.switch_to.window(self.browser.window_handles[1])
 
-            browser.switch_to.window(browser.window_handles[1])
+            self.browser.find_element(By.NAME, 'OTP').send_keys(zohoOTPCode)
+            self.browser.find_element(By.ID, 'nextbtn').click()
 
-            browser.find_element_by_name('OTP').send_keys(zohoOTPCode)
-            browser.find_element_by_id('nextbtn').click()
-            browser.implicitly_wait(10)
 
-            # code = browser.find_element_by_xpath()
+        self.browser.execute_script("window.open('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin');") 
+        #proceed with new tab
+        self.browser.switch_to.window(self.browser.window_handles[1])
+        
+        WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((By.ID, 'identifierId')))
+        gmailUserBox = self.browser.find_element_by_id('identifierId')
+        gmailUserBox.send_keys(self.email)
 
-            #enter email
-            #press next
-            #skip
-            
-            # tiktokUser = browser.find_element_by_xpath('//*[@id="root"]/div/div[1]/form/div/div[2]/input').text
-            # browser.find_element_by_xpath('//*[@id="root"]/div/div[1]/form/button[1]').click()
-            hangingStall = input("Hanging")
-        except:
-            print("\nbrowser abruptly closed")
+        WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="identifierNext"]/div/button')))
+        self.browser.find_element(By.XPATH, '//*[@id="identifierNext"]/div/button').click()
+        self.browser.implicitly_wait(10)
 
-    if __name__ == "__main__":
-        main()
+        WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((By.ID, 'identifierId')))
+        gmailPasswBox = self.browser.find_element(By.NAME, 'password')
+        gmailPasswBox.send_keys(self.passw)
+
+        WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="passwordNext"]/div/button')))
+        self.browser.find_element(By.XPATH, '//*[@id="passwordNext"]/div/button').click()
+
+        
+    #Zoho Signin
+    def zohoSignIn(self):
+        self.browser.execute_script("window.open('https://accounts.zoho.com/signin?servicename=VirtualOffice&signupurl=https://www.zoho.com/mail/zohomail-pricing.html&serviceurl=https://mail.zoho.com', 'zohoSignIN');")
+        #proceed with new tab
+        self.browser.switch_to.window('zohoSignIN')
+
+        WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((By.NAME, 'LOGIN_ID')))
+        self.browser.find_element(By.NAME, 'LOGIN_ID').send_keys(self.email)
+        self.browser.find_element(By.ID, 'nextbtn').click()   
+
+
+    #Wait for Google Signin and grab code from email
+    def getRecentCode(self):
+        self.googleSignIn()
+        time.sleep(10)
+        self.browser.refresh()
+        self.browser.find_element(By.XPATH, "/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div[8]/div/div[1]/div[3]/div/table/tbody/tr[1]/td[5]/div[2]/span[1]/span").click()
+
+        zohoOTPCode = self.browser.find_element(By.XPATH, '/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tr/td[1]/div[2]/div[2]/div/div[3]/div[2]/div/div/div/div/div[1]/div[2]/div[3]/div[3]/div/div[1]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[4]/td/b').text
+        print(zohoOTPCode)
+        return zohoOTPCode 
+
+    def main(self):
+        # self.browser.maximize_window()
+        self.tiktokAccountCreate()
+
+if __name__ == "__main__":
+
+    data = open("settings.json", "r+", encoding="utf8")
+    jsondata = json.load(data)
+    data.close()
+
+    f = open(os.getcwd()+"/emailList.csv", 'w')
+    csvdata = f.readlines()
+
+    newChrome = My_Chrome(jsondata, csvdata)
+    newChrome.main()
+
+    hangingStall = input("Hanging")
